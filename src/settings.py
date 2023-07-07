e4a35ae19b385d80fb2eaa68c4832d6ba64416f3
Your Name <you@youraddress.com>
2023-07-07 17:51:28 +0500

    first commit

--- src/settings.py
+++ src/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!1fqeo*mu@-omxc*$_x6@ww4^1sz1wye!4rgl59@#6c7o*zo-h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
